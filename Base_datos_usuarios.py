71ca84b8b43f7a12df759d683f8edaeb806b0752
lettopk <mtarazona97@uan.edu.co>
2024-07-16 13:10:25 -0500

    conexion a base de datos exitosa

--- Base_datos_usuarios.py
+++ Base_datos_usuarios.py
@@ -18,23 +18,34 @@ from pymongo import MongoClient
 # Conectar a MongoDB
 try:
     client = MongoClient('mongodb://localhost:27017/')
-    db = client.Admins  # Reemplaza 'mydatabase' con tu nombre de base de datos
+    db = client['GestorLaGrieta'] # Reemplaza 'mydatabase' con tu nombre de base de datos
     print("Conexión exitosa a MongoDB")
 except Exception as e:
     print(f"No se pudo conectar a MongoDB: {e}")
+
+
+#Verificar conexion
+try:
+    # El siguiente comando intentará recuperar la lista de bases de datos y fallará si la conexión no está disponible
+    client.admin.command('ping')
+    print("Conexión a MongoDB exitosa")
+except Exception as e:
+    print(f"Error al conectar a MongoDB: {e}")
 
 # Definir la colección
 usuarios = db['usuarios']
 
 # Crear documentos de ejemplo e insertarlos en la colección
 usuario1 = {
-    'id_usuario': 1,
-    'username': 'usuario1',
-    'password': 'password1'
+    "id_usuario": 1,
+    "username": "usuario1",
+    "password": "password1"
 }
+print("usuario1 para agregar")
 
 usuarios.insert_one(usuario1)
 
+print("usuario1 agregado")
 
 # Consultar todos los usuarios
 for usuario in usuarios.find():
