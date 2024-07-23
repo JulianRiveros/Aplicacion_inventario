7a346b478909ad8d85c8f20535ed69a433f89f8c
Julian <julian199664@gmail.com>
2024-07-22 20:59:44 -0500

    Creacion de CRUD para la clase usuario

--- Menu.py
+++ Menu.py
@@ -1,15 +1,21 @@
 import Base_datos_usuarios
 import getpass
+from colorama import init, Fore, Back, Style
 
+
+##Funcion para modificar la contraseña del usuario que inicio sesion
+#def modificar_contraseña_usuario():
 
 ##Funcion para mostrar el menu luego de inicio de sesion
-def mostrar_menu_usuarios():
+def mostrar_menu_usuarios(inicio_sesion, usuarios):
     while True:
         print("\nOpciones:")
         print("1. Gastos")
         print("2. Ingresos")
         print("3. Inventario")
-        print("4. Salir")
+        print("4. Modificar contraseña Usuario")
+        print("5. Eliminar Usuario")
+        print("6. Cerrar Sesion")
         opcion = input ("Seleccione una opcion: ")
         
         if opcion == "1":
@@ -19,9 +25,23 @@ def mostrar_menu_usuarios():
         elif opcion == "3":
             print("Se ingresa exitosamente al menu de Inventario")
         elif opcion == "4":
+            while True:
+                nuevaContrasenia = getpass.getpass(prompt="Ingrese la nueva contraseña: ") ## Se oculta la contraseña mientras se ingresa
+                validacionContrasenia = getpass.getpass(prompt="Ingrese de nuevo la contraseña: ") ## Se oculta la contraseña mientras se ingresa
+                if(nuevaContrasenia == validacionContrasenia):
+                    Base_datos_usuarios.modificar_usuario(inicio_sesion.nombreUsuario, nuevaContrasenia,usuarios)
+                    break
+                else:
+                    print(Fore.RED + "Las contraseñas no coinciden")
+        elif opcion == "5":
+            if inicio_sesion:
+                Base_datos_usuarios.eliminar_usuario(usuarios)
+            else:
+                print(Fore.RED + "No ha iniciado sesion")    
+        elif opcion == "6":
             break
         else:
-            print("Opcion no valida, intente nuevamente")
+            print(Fore.RED + "Opcion no valida, intente nuevamente")
             
 
 
@@ -32,9 +52,10 @@ def mostrar_menu_principal(usuarios):
         print("1. Registrar usuario")
         print("2. Iniciar sesión")
         print("3. Consultar Usuarios")
-        print("4. Salir")
-        opcion = input ("Seleccione una opcion: ")
-
+        print(Fore.RED + "4. Salir")
+        print(Style.RESET_ALL)
+        opcion = input (Back.CYAN + Fore.BLACK + "Seleccione una opcion: ")
+        print(Style.RESET_ALL)
         if opcion == "1":
             nombre_usuario= input("Ingrese nombre de usuario: ")
             #contrasenia = input("Ingrese contraseña: ")
@@ -46,7 +67,7 @@ def mostrar_menu_principal(usuarios):
             contrasenia = getpass.getpass(prompt="Ingresa tu contraseña: ")  ## Se oculta la contraseña mientras se ingresa
             inicio_sesion = Base_datos_usuarios.iniciar_sesion(nombre_usuario, contrasenia, usuarios)
             if inicio_sesion:
-                mostrar_menu_usuarios()
+                mostrar_menu_usuarios(inicio_sesion,usuarios)
         elif opcion == "3":
             # Consultar todos los usuarios
             for usuario in usuarios.find():
@@ -54,7 +75,7 @@ def mostrar_menu_principal(usuarios):
         elif opcion == "4":
             break
         else:
-            print("Opcion no valida. Intente nuevamente.")
+            print(Fore.RED + "Opcion no valida. Intente nuevamente.")
 
 if __name__ == "__main__":
     usuarios = Base_datos_usuarios.crear_conexion_base_datos()
